76bb6869a4bd94b74ece7b0a4b307829441e138a
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-04-21 00:27:49 +0300

    message controller implemented

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages)
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
