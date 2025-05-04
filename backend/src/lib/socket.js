c090817b8cec2d956eef063f8778357d76b8088d
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-05-04 23:18:38 +0300

    updated needed files

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,8 +7,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chat-app-m154.onrender.com"],
-  },
+    origin: [
+      "http://localhost:5173", 
+      "https://chat-app-m154.onrender.com"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 export function getReceiverSocketId(userId) {
